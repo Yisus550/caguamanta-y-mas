d0cf03503ee88ec107b3f29655c8c1edacae357f
Yisus550 <jesus.alfonso2402@gmail.com>
2024-09-24 17:14:12 -0700

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace caguamanta_y_mas.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
